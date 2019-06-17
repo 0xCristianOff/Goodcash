291730b78ebfdd50920c680ef32cc3d348547db2
root <ivighenie@yahoo.com>
2019-06-17 10:14:24 -0400

    Added correct source code

--- INSTALL.md
+++ INSTALL.md
@@ -1,5 +1,5 @@
-Building Litecoin
+Building Goodcash
 ================
 
 See doc/build-*.md for instructions on building the various
-elements of the Litecoin Core reference implementation of Litecoin.
+elements of the Goodcash Core reference implementation of Goodcash.
--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,15 +7,15 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-LITECOIND=${BITCOIND:-$BINDIR/litecoind}
-LITECOINCLI=${BITCOINCLI:-$BINDIR/litecoin-cli}
-LITECOINTX=${BITCOINTX:-$BINDIR/litecoin-tx}
-LITECOINQT=${BITCOINQT:-$BINDIR/qt/litecoin-qt}
+LITECOIND=${BITCOIND:-$BINDIR/goodcashd}
+LITECOINCLI=${BITCOINCLI:-$BINDIR/goodcash-cli}
+LITECOINTX=${BITCOINTX:-$BINDIR/goodcash-tx}
+LITECOINQT=${BITCOINQT:-$BINDIR/qt/goodcash-qt}
 
 [ ! -x $LITECOIND ] && echo "$LITECOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-LTCVER=($($LITECOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+GCASHVER=($($LITECOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
@@ -25,8 +25,8 @@ $LITECOIND --version | sed -n '1!p' >> footer.h2m
 
 for cmd in $LITECOIND $LITECOINCLI $LITECOINTX $LITECOINQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${LTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${LTCVER[1]}//g" ${MANDIR}/${cmdname}.1
+  help2man -N --version-string=${GCASHVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${GCASHVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
